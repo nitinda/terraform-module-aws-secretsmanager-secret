ce62c51d5b40e6c517e6e17ad894827fc5d8957b
nitinda <nitinda@gmail.com>
2020-04-24 17:40:48 +0100

    Modified Code

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
 output "id" {
-  value = "aws_secretsmanager_secret.secretsmanager_secret.id"
+  value = aws_secretsmanager_secret.secretsmanager_secret.id
 }
 
 output "arn" {
-  value = "aws_secretsmanager_secret.secretsmanager_secret.arn"
+  value = aws_secretsmanager_secret.secretsmanager_secret.arn
 }
 
 output "rotation_enabled" {
-  value = "aws_secretsmanager_secret.secretsmanager_secret.rotation_enabled"
+  value = aws_secretsmanager_secret.secretsmanager_secret.rotation_enabled
 }
